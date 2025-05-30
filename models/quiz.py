1524ec9d1284c040150814928b0c6c107e079d1a
AndrewPolyak <andrew.polyak23@gmail.com>
2025-05-30 17:26:02 -0600

    Improvements to multi-choice quiz generation

--- models/quiz.py
+++ models/quiz.py
@@ -6,6 +6,10 @@ from groq import Groq
 
 
 class MultiChoiceQuiz():
+    """
+    This class provides `generate_quiz()`, an interface to generate a multiple choice quiz
+    with the specifed number of questions within the provided academic context.
+    """
     def __init__(self):
         super().__init__()
 
@@ -16,6 +20,40 @@ class MultiChoiceQuiz():
     
     
     def generate_quiz(self, quiz_context: str, num_questions: int) -> str:
+        """
+        This function returns a JSON string representing the multiple choice quiz.
+        The quiz is generated based on the quiz context, and the number of questions
+        are specified by the function caller.
+        
+        Args:
+            quiz_context: str --> The academic context the quiz will be based on
+            num_questions: int --> The number of questions asked within the quiz
+        
+        Returns:
+            A JSON in the following format:
+
+            {
+                "question1": {
+                    "Question": "Text prompt for question 1",
+                    "A": "Text is first option for question 1",
+                    "B": "Text is first option for question 1",
+                    "C": "Text is first option for question 1",
+                    "D": "Text is first option for question 1",
+                    "Answer": "Text is answer for question 1",
+                    "Explanation": "Text is explanation for answer"
+                },
+                "question2": {
+                    "Question": "Text prompt for question 2",
+                    "A": "Text is first option for question 2",
+                    "B": "Text is first option for question 2",
+                    "C": "Text is first option for question 2",
+                    "D": "Text is first option for question 2",
+                    "Answer": "Text is answer for question 2",
+                    "Explanation": "Text is explanation for answer"
+                }
+            }
+        """
+
         self.set_num_questions(num_questions=num_questions)
         
         prompt = f"""
@@ -49,34 +87,72 @@ class MultiChoiceQuiz():
     
 
     def get_schema(self) -> str:
+        """
+        Getter function for the LLM JSON schema prompt
+
+        Args:
+            None
+        
+        Returns:
+            The LLM JSON schema prompt
+        """
+
         schema = json.load(open("models/mutli-choice-quiz-schema.json", "r")) # TODO Fix file path by release
 
         return schema
 
 
     def get_system_prompt(self) -> str:
+        """
+        Getter function for the LLM system prompt
+
+        Args:
+            None
+        
+        Returns:
+            The system prompt for the LLM
+        """
+
         example_output = """
             "question1": {
                 "Question": "Where is the city of Calgary located?",
                 "A": "Montana, USA",
                 "B": "Saskatchewan, Canada",
                 "C": "Alberta, Canada",
-                "D": "Washington, USA"
+                "D": "Washington, USA",
+                "Answer": "C",
+                "Explanation": "Calgary is located within the Canadian province of Alberta."
             },
             "question2": {
                 "Question": "Where is the city of Vancouver located?",
                 "A": "Alberta, Canada",
                 "B": "British Colombia, Canada",
                 "C": "Texas, USA",
-                "D": "California, USA"
+                "D": "California, USA",
+                "Answer": "B",
+                "Explanation": "Vancouver is located within the Canadian province of British Colombia."
             },
             """
         
         system_prompt = f"""
             You are an university professor who specializes in making challenging, yet fair and intellectually stimulating
-            multiple choice quizzes. Your questions should force the test-taker to think, and all answers should seem equally
-            feasible, prompting the user to think deeply and weigh their options, not see an obvious choice immediately.
+            multiple choice quizzes.
 
+            You must design your quiz around the following numbered principles:
+            1. Instruct students to select the “best answer” rather than the “correct answer”. By doing this, you acknowledge the fact that the distractors may have an element of truth to them and discourage arguments from students who may argue that their answer is correct as well.
+            2. Use familiar language. The question should use the same terminology that was used in the course. Avoid using unfamiliar expressions or foreign language terms, unless measuring knowledge of such language is one of the goals of the question. Students are likely to dismiss distractors with unfamiliar terms as incorrect.
+            3. Avoid giving verbal association clues from the stem in the key. If the key uses words that are very similar to words found in the stem, students are more likely to pick it as the correct answer.
+            4. Avoid trick questions. Questions should be designed so that students who know the material can find the correct answer. Questions designed to lead students to an incorrect answer, through misleading phrasing or by emphasizing an otherwise unimportant detail of the solution, violate this principle.
+            5. Make sure there is only one best answer. Avoid having two or more options that are correct, but where one is “more” correct than the others. The distractors should be incorrect answers to the question posed in the prompt.
+            6. Make the distractors appealing and plausible. If the distractors are farfetched, students will too easily locate the correct answer, even if they have little knowledge. When testing for recognition of key terms and ideas keep the distractors similar in length and type of language as the correct solution. When testing conceptual understanding, distractors should represent common mistakes made by students.
+            7. Randomly distribute the correct response. The exam should have roughly the same number of correct answers that are a's, b's, c's, and d's.
+            8. Avoid using “all of the above”. If “all of the above” is an option and students know two of the options are correct, the answer must be “all of the above”. If they know one is incorrect, the answer must not be “all of the above”. A student may also read the first option, determine that it is correct, and be misled into choosing it without reading all of the options.
+            9. Avoid negative wording. Students often fail to observe negative wording and it can confuse them. As a result, students who are familiar with the material often make mistakes on negatively worded questions. In general, avoid having any negatives in the stem or the options.
+            10. Make sure distractors match the correct answer in terms of length, complexity, phrasing, and style.
+            11. Avoid language in the options and stems that clues the correct answer.
+            12. Make options similar in grammar, length, complexity, and style.
+            13. Ensure questions are equally distributed across the provided course context and don't solely focus on a single topic.
+            
             You will recieve the course content from which you should strictly base the quiz on. You should not include
             content or concepts not mentioned in the provided content.
 
@@ -96,16 +172,37 @@ class MultiChoiceQuiz():
     
 
     def set_num_questions(self, num_questions: int) -> None:
+        """
+        Setter function for the number of questions
+
+        Args:
+            num_questions: int --> The number of questions to be generated in the quiz
+        
+        Returns:
+            None
+        """
         self.num_questions = num_questions
     
 
     def get_num_questions(self) -> int:
+        """
+        Getter function for the number of questions
+
+        Args:
+            None
+        
+        Returns:
+            The number of questions to be generated in the quiz
+        """
         return self.num_questions
 
 
+# Test
 def main():
-    quiz_generator = MultiChoiceQuiz()
 
+    quiz_generator_mc = MultiChoiceQuiz()
+
+    # Random historical info on computers
     text = """
         The history of computers spans centuries and reflects humankind's evolving ability to process information and solve complex problems. Early computational devices, such as the abacus (used as early as 2400 BCE in Mesopotamia), helped people perform basic arithmetic. In the 17th century, inventors like Blaise Pascal and Gottfried Wilhelm Leibniz developed mechanical calculators capable of performing addition, subtraction, multiplication, and division. These innovations laid the foundation for future advancements in computing.
 
@@ -116,7 +213,14 @@ def main():
         In the decades that followed, computing technology advanced rapidly. The invention of the transistor in 1947 revolutionized computer design by replacing bulky vacuum tubes, enabling smaller and more reliable machines. Integrated circuits in the 1960s and microprocessors in the 1970s further accelerated this trend. Personal computers emerged in the late 1970s and early 1980s, with companies like Apple, IBM, and Microsoft shaping the modern computing landscape. Today’s computers are millions of times more powerful than their early ancestors and have become essential tools in nearly every aspect of life, from communication and entertainment to science and industry.
         """
 
-    print(quiz_generator.generate_quiz(quiz_context=text, num_questions=5))
+    # Multi-Choice
+    print(f"Multiple choice quiz:\n{quiz_generator_mc.generate_quiz(quiz_context=text, num_questions=5)}\n\n")
+
+    # True/False
+    # TODO
+
+    # Fill-in-the-Blank
+    # TODO
 
 
 if __name__== "__main__":
